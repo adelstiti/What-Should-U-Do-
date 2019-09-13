6a2f7dc17b9070b816a33385b213e1f2ceee2774
Adel Stiti <adelstiti@users.noreply.github.com>
2019-09-13 23:07:39 +0100

    Updates

---
+++ precache-manifest.f46766288ce31f4ad04a2663c9c19942.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76dbfcb2e45196aa17d0fb7938e10e35",
+    "url": "/What-Should-U-Do-/index.html"
+  },
+  {
+    "revision": "2946db3774b1ca4705d2",
+    "url": "/What-Should-U-Do-/static/css/2.f2987ecf.chunk.css"
+  },
+  {
+    "revision": "97e4eb45e4c33f4f88fc",
+    "url": "/What-Should-U-Do-/static/css/main.610b16ca.chunk.css"
+  },
+  {
+    "revision": "2946db3774b1ca4705d2",
+    "url": "/What-Should-U-Do-/static/js/2.1f538691.chunk.js"
+  },
+  {
+    "revision": "97e4eb45e4c33f4f88fc",
+    "url": "/What-Should-U-Do-/static/js/main.d08518d6.chunk.js"
+  },
+  {
+    "revision": "ed55cfadf43af63aa168",
+    "url": "/What-Should-U-Do-/static/js/runtime~main.bc54ebfd.js"
+  }
+]);
